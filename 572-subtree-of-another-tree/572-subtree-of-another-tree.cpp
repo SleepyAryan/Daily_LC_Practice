e7ce9f3862929b7146c59971834faf2609386b80
Aryan Dash <87265013+SleepyAryan@users.noreply.github.com>
2022-04-20 12:39:41 +0530

    Time: 39 ms (35.61%), Space: 28.8 MB (83.10%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -18,15 +18,18 @@ public:
         if(!root or !subRoot){
             return false;
         }
+        //check if the root ka value is equal and also check for the left node and the right node
         return root->val==subRoot->val and equal(root->left,subRoot->left) and equal(root->right,subRoot->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        //this is generic stuff...
         if(!root and !subRoot){
             return true;
         }
         if(!root or !subRoot){
             return false;
         }
+        //check if its equal and also theres a high chance that its not equal so just push the left and the right
         return equal(root,subRoot) || isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
 };
